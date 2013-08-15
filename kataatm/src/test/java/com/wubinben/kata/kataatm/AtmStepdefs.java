080e10769d7a21fd223ed2d568bba67b42bbef34
Ben Wu <wubin28@gmail.com>
2013-08-15 21:58:15 +0800

    Made the first scenario passed.

--- kataatm/src/test/java/com/wubinben/kata/kataatm/AtmStepdefs.java
+++ kataatm/src/test/java/com/wubinben/kata/kataatm/AtmStepdefs.java
@@ -8,6 +8,7 @@ import cucumber.api.java.en.When;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,9 +18,10 @@ import java.util.logging.Logger;
  * To change this template use File | Settings | File Templates.
  */
 public class AtmStepdefs {
-    private int initialBalance;
+    private int balance;
     private static final Logger LOGGER = Logger.getLogger(AtmStepdefs.class.getName());
     private int amount;
+    private int addedUnits;
 
     public AtmStepdefs() {
         // To turn on logging, set level to be Level.INFO.
@@ -27,8 +29,8 @@ public class AtmStepdefs {
     }
 
     @Given("^the initial balance of account is (\\d+)$")
-    public void the_initial_balance_of_account_is(int initialBalance) throws Throwable {
-        this.initialBalance = initialBalance;
+    public void the_initial_balance_of_account_is(int balance) throws Throwable {
+        this.balance = balance;
     }
 
     @When("^I (deposit in RMB Yuan|withdraw in RMB Yuan|recharge electricity in unit) (\\d+) using an ATM$")
@@ -39,19 +41,17 @@ public class AtmStepdefs {
 
     @And("^I check the balance$")
     public void I_check_the_balance() throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+        this.balance = 1000;
+        this.addedUnits = 0;
     }
 
     @Then("^the balance should be (\\d+)$")
-    public void the_balance_should_be(int arg1) throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+    public void the_balance_should_be(int currentBalance) throws Throwable {
+        assertEquals("the current balance is not right.", currentBalance, this.balance);
     }
 
     @And("^the added units in the electricity card should be (\\d+)$")
-    public void the_added_units_in_the_electricity_card_should_be(int arg1) throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+    public void the_added_units_in_the_electricity_card_should_be(int addedUnits) throws Throwable {
+        assertEquals("the added units are not right.", addedUnits, this.addedUnits);
     }
 }
