875c37bd02fea611771773ac698eec462a361132
Ben Wu <wubin28@gmail.com>
2013-08-16 12:12:53 +0800

    Delegated business logic to AtmFacade object.

--- kataatm/src/main/java/com/wubinben/kata/kataatm/AtmFacade.java
+++ kataatm/src/main/java/com/wubinben/kata/kataatm/AtmFacade.java
@@ -8,11 +8,33 @@ package com.wubinben.kata.kataatm;
  * To change this template use File | Settings | File Templates.
  */
 public class AtmFacade {
+    private int balance;
+    private int addedUnits;
+
     public static AtmFacade newInstance() {
         return new AtmFacade();
     }
 
     public void doSelfService(String action, int amount) {
-        //To change body of created methods use File | Settings | File Templates.
+        if (action.equals("deposit in RMB Yuan")) {
+            this.balance += amount;
+        } else if (action.equals("withdraw in RMB Yuan")) {
+            this.balance -= amount;
+        } else if (action.equals("recharge electricity in unit")) {
+            this.addedUnits = amount;
+            this.balance -= amount * 2;
+        }
+    }
+
+    public void setOldBalance(int oldBalance) {
+        this.balance = oldBalance;
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public int getAddedUnits() {
+        return this.addedUnits;
     }
 }
--- kataatm/src/test/java/com/wubinben/kata/kataatm/AtmStepdefs.java
+++ kataatm/src/test/java/com/wubinben/kata/kataatm/AtmStepdefs.java
@@ -1,6 +1,5 @@
 package com.wubinben.kata.kataatm;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -18,10 +17,7 @@ import static org.junit.Assert.assertEquals;
  * To change this template use File | Settings | File Templates.
  */
 public class AtmStepdefs {
-    private int balance = 0;
     private static final Logger LOGGER = Logger.getLogger(AtmStepdefs.class.getName());
-    private int amount;
-    private int addedUnits = 0;
     private AtmFacade atmFacade = AtmFacade.newInstance();
 
     public AtmStepdefs() {
@@ -31,32 +27,22 @@ public class AtmStepdefs {
 
     @Given("^the old balance of my account is (\\d+)$")
     public void the_old_balance_of_my_account_is(int oldBalance) throws Throwable {
-        this.balance = oldBalance;
+        this.atmFacade.setOldBalance(oldBalance);
     }
 
     @When("^I (deposit in RMB Yuan|withdraw in RMB Yuan|recharge electricity in unit) (\\d+) using an ATM$")
     public void I_do_self_service(String action, int amount) throws Throwable {
-        this.amount = amount;
         LOGGER.info(">>action: " + action);
-
         this.atmFacade.doSelfService(action, amount);
-        if (action.equals("deposit in RMB Yuan")) {
-            this.balance += amount;
-        } else if (action.equals("withdraw in RMB Yuan")) {
-            this.balance -= amount;
-        } else if (action.equals("recharge electricity in unit")) {
-            this.addedUnits = amount;
-            this.balance -= amount * 2;
-        }
     }
 
     @Then("^the new balance should be (\\d+)$")
     public void the_new_balance_should_be(int newBalance) throws Throwable {
-        assertEquals("the new balance is not right.", newBalance, this.balance);
+        assertEquals("the new balance is not right.", newBalance, this.atmFacade.getBalance());
     }
 
     @And("^the added units in the electricity card should be (\\d+)$")
     public void the_added_units_in_the_electricity_card_should_be(int addedUnits) throws Throwable {
-        assertEquals("the added units are not right.", addedUnits, this.addedUnits);
+        assertEquals("the added units are not right.", addedUnits, this.atmFacade.getAddedUnits());
     }
 }
